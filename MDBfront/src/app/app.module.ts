768c62df1722e4d93faa74259bcaa4a7365bd4fd
Quinn Bierbaum <quinn.bierbaum@gmail.com>
2020-04-13 16:18:14 -0400

    Added HTTP Client import

--- MDBfront/src/app/app.module.ts
+++ MDBfront/src/app/app.module.ts
@@ -7,6 +7,7 @@ import { RegisterComponent } from './register/register.component';
 
 import { LoginComponent } from './login/login.component' 
 import { FormsModule, ReactiveFormsModule } from '@angular/forms'; // <-- NgModel lives here
+import { HttpClientModule } from '@angular/common/http'; 
 
 @NgModule({
   declarations: [
@@ -18,7 +19,8 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms'; // <-- NgMode
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
