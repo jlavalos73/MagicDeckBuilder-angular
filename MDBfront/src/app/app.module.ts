bf6de160710fd2cebc427764f54003baf507f636
Quinn Bierbaum <quinn.bierbaum@gmail.com>
2020-04-13 16:44:07 -0400

    created component

--- MDBfront/src/app/app.module.ts
+++ MDBfront/src/app/app.module.ts
@@ -7,13 +7,15 @@ import { RegisterComponent } from './register/register.component';
 
 import { LoginComponent } from './login/login.component' 
 import { FormsModule, ReactiveFormsModule } from '@angular/forms'; // <-- NgModel lives here
-import { HttpClientModule } from '@angular/common/http'; 
+import { HttpClientModule } from '@angular/common/http';
+import { DeckViewComponent } from './deck-view/deck-view.component'; 
 
 @NgModule({
   declarations: [
     AppComponent,
     RegisterComponent,
-    LoginComponent
+    LoginComponent,
+    DeckViewComponent
   ],
   imports: [
     BrowserModule,
---
+++ MDBfront/src/app/deck-view/deck-view.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-deck-view',
+  templateUrl: './deck-view.component.html',
+  styleUrls: ['./deck-view.component.css']
+})
+export class DeckViewComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
