ea15667c7a204a299088d9efba154ada4db12974
Jonathan Conner <jonathan.g.conner@gmail.com>
2020-04-13 15:59:42 -0400

    Added new application routes and Route Guards for Auth

--- MDBfront/src/app/app-routing.module.ts
+++ MDBfront/src/app/app-routing.module.ts
@@ -1,8 +1,35 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { AuthGuard } from './guards/auth.guard';
+import { MDBGuard } from './guards/mdb.guard';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  //Default redirect
+  {
+     path: '',
+     pathMatch: 'full',
+     redirectTo: '/login'
+  },
+
+  //login route
+  {
+    path: 'login',
+    component: LoginComponent,
+    canActivate: [AuthGuard]
+  },
+
+  //MDB module route
+  {
+    path: '/mdb/app',
+    loadChildren: './mdb/mdb.module#MDBModule',
+    canActivate: [MDBGuard],
+    canLoad: [MDBGuard]
+  }
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
