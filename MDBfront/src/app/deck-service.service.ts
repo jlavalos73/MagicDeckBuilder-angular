8ad2e7d7143e908a90698578a08e0be9e14601cd
The Camel <jcfo233@gmail.com>
2020-04-16 18:29:43 -0500

    Add decks

--- MDBfront/src/app/deck-service.service.ts
+++ MDBfront/src/app/deck-service.service.ts
@@ -1,15 +1,20 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Deck } from './models/deck';
+import { Observable } from 'rxjs';
 
 
 @Injectable({
   providedIn: 'root'
 })
 export class DeckServiceService {
+  addDeck(deck: Deck): Observable<Deck> {
+    return this.http.post<Deck>('http://54.211.173.35:8085/MDB/deck', deck)
+  }
 
   constructor(private http:  HttpClient) { }
 
   getDeck(id: number){
-    return this.http.get(`http://54.211.173.35:8085/MDBback/auth/${id}`)
+    return this.http.get(`http://54.211.173.35:8085/MDB/deck/${id}`)
   }
 }
--- MDBfront/src/app/deck-view/deck-view.component.ts
+++ MDBfront/src/app/deck-view/deck-view.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { User } from 'src/app/models/user';
 import { Card } from 'src/app/models/card';
 import { Deck } from 'src/app/models/deck';
+import { DeckServiceService } from '../deck-service.service';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-deck-view',
@@ -13,8 +15,29 @@ export class DeckViewComponent{
   currentUser: User
   decks: Deck[] = JSON.parse(localStorage.getItem('currentUser')).Decks;
   currentDeck: Deck = null;
+  uploadForm: FormGroup;
+  newDeck: Deck;
+  constructor(
+    private deckserv: DeckServiceService,
+    private formBuilder: FormBuilder
+  ){}
+
+  ngOnInit(): void {
+    this.uploadForm = this.formBuilder.group({
+      name: ""
+    })
+    this.currentUser = JSON.parse(localStorage.getItem("currentUser"))
+  }
   setCurrentDeck(d){
     this.currentDeck = d;
+  }
+
+  onSubmit(){
+    this.newDeck.name = this.uploadForm.controls.name.value;
+    this.newDeck.user = JSON.parse(localStorage.getItem("currentUser"))
+    this.deckserv.addDeck(this.newDeck).subscribe(value => this.newDeck);
+    this.currentUser.decks.push(this.newDeck);
+    localStorage.setItem('currentUser', JSON.stringify(this.currentUser))
   }
 
 }
